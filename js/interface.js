2c91a3c6f37ed37490fc2fdadcf060e973775a6b
awye765 <awye765@googlemail.com>
2016-06-21 21:47:20 +0100

    Added additional annotation to explain JQuery use of attr keyword to set CSS for HTML elements

--- js/interface.js
+++ js/interface.js
@@ -44,11 +44,27 @@ $(document).ready(function() {
 
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
+    // Replaces the html tag with id 'temperature' with whatever is the return
+    // value of the JS method thermostat.temperature in our thermostat.js file.
+    // This allows us to display the current temperature of the thermostat in the
+    // browser.
+    //
     $('#temperature').attr('class', thermostat.energyUsage());
+    // The above syntax takes the pattern:
+    // => $(selector).attr(attribute, value)
+    //
+    // Attr sets the attribute and value for the selector.  In the above:
+    // => the selector is the html in our html file with id of 'temperature'
+    // => the attribute in the above is 'class'
+    // => the value in the above is the return value of 'thermostat.energyUsage'
+    //
+    // The return value of thermostat.energyUsage() will return a string, either
+    // "low-usage", "medium-usage" or "high-usage".  Those strings correspond
+    // to the names of the CSS classes in the CSS file.  Together with the
+    // attribute 'class' in the above, this tells JQuery to render the html
+    // element with id of "temperature" in the html file in the appropriate
+    // colour described in the CSS file.
+    //
   }
-  // Replaces the html tag with id 'temperature' with whatever is the return
-  // value of the JS method thermostat.temperature in our thermostat.js file.
-  // This allows us to display the current temperature of the thermostat in the
-  // browser.
 
 })
